99f6889b2d00123b964619b0ce46e7255bda8945
Anton Donchenko <donch.a080@gmail.com>
2025-06-16 15:06:27 +0300

    solution fix

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,26 +1,102 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li>
-        <img src="./img/1.png" alt="1" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="2" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="3" />
-      </li>
-      <li>
-        <img src="./img/1.png" alt="4" />
-      </li>
-    </ul>
+type Props = {
+  images: string[];
+  step: number;
+  frameSize: number;
+  itemWidth: number;
+  animationDuration: number;
+  infinite?: boolean;
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+enum Direction {
+  next = 'next',
+  prev = 'prev',
+}
+
+const Carousel: React.FC<Props> = ({
+  images,
+  step,
+  frameSize,
+  itemWidth,
+  animationDuration,
+}) => {
+  const listRef = useRef<HTMLUListElement>(null);
+  const [offset, setOffset] = useState(0);
+
+  useEffect(() => {
+    setOffset(0);
+    document.title = 'Carousel';
+    if (listRef.current) {
+      listRef.current.style.transform = `translateX(0px)`;
+    }
+  }, [frameSize, images.length]);
+
+  function scrollToImage(direction: Direction) {
+    const listNode = listRef.current;
+    let newOffset =
+      direction === Direction.next ? offset + step : offset - step;
+
+    if (newOffset >= images.length - 1) {
+      newOffset = images.length - frameSize;
+    }
+
+    if (newOffset < 0) {
+      newOffset = 0;
+    }
+
+    if (listNode) {
+      listNode.style.transform = `translateX(-${newOffset * itemWidth}px)`;
+      setOffset(newOffset);
+    }
+  }
+
+  return (
+    <div className="Carousel">
+      <div
+        className="Carousel__wrapper"
+        style={{ width: frameSize * itemWidth }}
+      >
+        <ul
+          className="Carousel__list"
+          style={{
+            width: 1300,
+            transition: `transform ${animationDuration}ms ease-in-out`,
+          }}
+          ref={listRef}
+        >
+          {images.map((image, index) => {
+            return (
+              <li key={image}>
+                <img
+                  src={image}
+                  alt={`${index + 1}`}
+                  style={{ width: itemWidth }}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <button
+        type="button"
+        onClick={() => scrollToImage(Direction.prev)}
+        disabled={offset === 0}
+      >
+        Prev
+      </button>
+      <button
+        data-cy="next"
+        type="button"
+        onClick={() => scrollToImage(Direction.next)}
+        disabled={offset === images.length - frameSize}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
 
 export default Carousel;
